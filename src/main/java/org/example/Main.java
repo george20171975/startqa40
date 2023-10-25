db85a11c44eb2aa2bd114c59493c0a1555333c89
george20171975 <gioiremadze75@gmail.com>
2023-10-25 20:19:19 +0300

    upd branch main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,5 @@ public class Main {
         System.out.println("Hello and welcome!");
         System.out.println();
         System.out.println("main");
-        System.out.println("mergeconflresolve");
     }
 }
